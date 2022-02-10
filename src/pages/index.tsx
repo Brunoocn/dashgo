f7903ee7fd4a1f9df711d5327025e98d5b89087a
Bruno Caneo <brunocaneo3@gmail.com>
2022-02-10 13:27:34 -0300

    Adding user create form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "../components/Form/Input";
 
 type SignInFormData = {
