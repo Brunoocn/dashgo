ca3db4341fe69181b06bcd6fdbf4a12837d8f95f
Bruno Caneo <brunocaneo3@gmail.com>
2022-02-22 15:35:01 -0300

    Adding pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { ActiveModelSerializer, createServer, Factory, Model, Response } from 'miragejs';
 import { faker } from '@faker-js/faker'
 
 type User = {
@@ -9,23 +9,25 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
-    models: {
-      user: Model.extend<Partial<User>>({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
 
-      })
+    models: {
+      user: Model.extend<Partial<User>>({})
     },
 
     factories: {
       user: Factory.extend({
-        name(i: number){
+        name(i: number) {
           return `User ${i + 1}`
         },
-        email(){
+        email() {
           return faker.internet.email().toLowerCase();
         },
-        createdAt(){
+        createdAt() {
           return faker.date.recent(10);
-        },
+        }
       })
     },
 
@@ -37,16 +39,16 @@ export function makeServer() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users', function(schema, request) {
-        const { page = 1, per_page = 10 } = request.queryParams
+      this.get('/users', function(schema, req) {
+        const { page = 1, per_page = 10 } = req.queryParams
 
         const total = schema.all('user').length
 
-        const pageStart = (Number(page) - 1) * Number(per_page);
-        const pageEnd = pageStart + Number(per_page);
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
 
-        const users = this.serialze(schema.all('user'))
-        .users.slice(pageStart, pageEnd)
+        const users = this.serialize(schema.all('user'))
+          .users.slice(pageStart, pageEnd)
 
         return new Response(
           200,
@@ -54,12 +56,14 @@ export function makeServer() {
           { users }
         )
       });
+      
+      this.get('/users/:id');
       this.post('/users');
 
       this.namespace = '';
-      this.passthrough() 
+      this.passthrough();
     }
-  });
+  })
 
   return server;
 }
